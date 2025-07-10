793390e4a71d043e30e62ce8244bcbae9eee9ae2
RodrigoMoreiraRibeiro <rodrigocontatos2002@gmail.com>
2025-07-10 18:59:38 -0300

    up requirements

--- bot.py
+++ bot.py
@@ -8,13 +8,21 @@ DISCORD_WEBHOOK_URL = os.environ["DISCORD_WEBHOOK_URL"]
 async def forward_to_discord(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message.text
     if msg:
+        print(f"[Telegram] Nova mensagem recebida: {msg}")
         data = {
-            "content": f"🤑 Nova Promoção na área! :\n{msg}"
+            "content": f"🤑 Nova Promoção na área!:\n{msg}"
         }
-        requests.post(DISCORD_WEBHOOK_URL, json=data)
+
+        response = requests.post(DISCORD_WEBHOOK_URL, json=data)
+
+        if response.status_code == 204:
+            print("[Discord] Mensagem enviada com sucesso!")
+        else:
+            print(f"[Discord] Erro ao enviar mensagem: {response.status_code} - {response.text}")
 
 if __name__ == "__main__":
     bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+    print("🤖 Bot Telegram → Discord iniciado.")
     app = ApplicationBuilder().token(bot_token).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_to_discord))
     app.run_polling()
